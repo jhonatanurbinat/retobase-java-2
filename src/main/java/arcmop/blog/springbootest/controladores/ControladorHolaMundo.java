fb46a5642cbbe3a4201540ed78de96e7b0244fb6
Jhonatan Paul Urbina Tolentino <53740985+jhonatanurbinat@users.noreply.github.com>
2025-05-13 18:11:47 -0500

    Update ControladorHolaMundo.java

--- src/main/java/arcmop/blog/springbootest/controladores/ControladorHolaMundo.java
+++ src/main/java/arcmop/blog/springbootest/controladores/ControladorHolaMundo.java
@@ -12,7 +12,7 @@ public class ControladorHolaMundo {
     @RequestMapping(value = "/sumar/{sum01}/{sum02}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
     Map saludar(@PathVariable("sum01") Integer sum01, @PathVariable("sum02") Integer sum02) {
-        return Collections.singletonMap("resultado", String.valueOf(sum01 + sum02));
+        return Collections.singletonMap("resultado", String.valueOf(sum01 * sum02));
     }
 
 
